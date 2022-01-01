ba18f65c14a240b323c765e4489e09811cf52c0f
pratch <82988679+pichayakorn@users.noreply.github.com>
2022-01-01 15:24:05 +0700

    initial commit

---
+++ Week_08_Menus_and_Pickers/PickerForTime/app/src/main/java/com/tni/pratch/pickerfortime/MainActivity.java
@@ -0,0 +1,30 @@
+package com.tni.pratch.pickerfortime;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showTimePicker(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), getString(R.string.timepicker));
+    }
+
+    public void processTimePickerResult(int hourOfDay, int minute) {
+        String dateMessage = String.format("%d:%02d", hourOfDay, minute);
+
+        // Show Toast
+        Toast.makeText(this, getString(R.string.time_message) + dateMessage,
+                Toast.LENGTH_SHORT).show();
+    }
+}
