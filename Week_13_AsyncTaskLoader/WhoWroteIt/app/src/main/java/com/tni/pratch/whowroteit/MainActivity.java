e522fac422373c0fd5fadfd6c457d11021826099
pratch <82988679+pichayakorn@users.noreply.github.com>
2022-02-18 17:08:14 +0700

    Part 3: Implement UI best practices

--- Week_13_AsyncTaskLoader/WhoWroteIt/app/src/main/java/com/tni/pratch/whowroteit/MainActivity.java
+++ Week_13_AsyncTaskLoader/WhoWroteIt/app/src/main/java/com/tni/pratch/whowroteit/MainActivity.java
@@ -2,8 +2,13 @@ package com.tni.pratch.whowroteit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -27,6 +32,46 @@ public class MainActivity extends AppCompatActivity {
     public void searchBooks(View view) {
         // Get the search string from the input field.
         String queryString = mBookInput.getText().toString();
-        new FetchBook(mTitleText, mAuthorText).execute(queryString);
+
+        // Hide the keyboard when the button is pushed.
+        InputMethodManager inputManager =
+                (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (inputManager != null ) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        // Check the status of the network connection.
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean hasInternet = false;
+        if (cm != null) {
+            Network[] networks = cm.getAllNetworks();
+            for (Network nw : networks) {
+                NetworkCapabilities nc = cm.getNetworkCapabilities(nw);
+                if (nc != null)
+                    if (nc.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET))
+                        hasInternet = true;
+            }
+        }
+        
+        // If the network is available, connected, and the search field is not empty,
+        // start a FetchBook AsyncTask.
+        if (hasInternet && queryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            // Otherwise update the TextView to tell the user there is no connection,
+            // or no search term.
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
     }
 }
