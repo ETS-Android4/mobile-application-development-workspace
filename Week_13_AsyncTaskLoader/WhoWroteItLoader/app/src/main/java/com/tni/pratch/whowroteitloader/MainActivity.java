0db45005e66e75a4acc71dc76ad0c465b887f1f9
pratch <82988679+pichayakorn@users.noreply.github.com>
2022-02-18 23:42:21 +0700

    Part 4: Migrate to AsyncTaskLoader

---
+++ Week_13_AsyncTaskLoader/WhoWroteItLoader/app/src/main/java/com/tni/pratch/whowroteitloader/MainActivity.java
@@ -0,0 +1,162 @@
+package com.tni.pratch.whowroteitloader;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<String>{
+
+    private EditText mBookInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize view
+        mBookInput = findViewById(R.id.bookInput);
+        mTitleText = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.authorText);
+
+        if(getSupportLoaderManager().getLoader(0) != null){
+            getSupportLoaderManager().initLoader(0,null,this);
+        }
+
+    }
+
+    public void searchBooks(View view) {
+        // Get the search string from the input field.
+        String queryString = mBookInput.getText().toString();
+
+        // Hide the keyboard when the button is pushed.
+        InputMethodManager inputManager =
+                (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (inputManager != null ) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        // Check the status of the network connection.
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean hasInternet = false;
+        if (cm != null) {
+            Network[] networks = cm.getAllNetworks();
+            for (Network nw : networks) {
+                NetworkCapabilities nc = cm.getNetworkCapabilities(nw);
+                if (nc != null)
+                    if (nc.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET))
+                        hasInternet = true;
+            }
+        }
+        
+        // If the network is available, connected, and the search field is not empty,
+        // start a FetchBook AsyncTask.
+        if (hasInternet && queryString.length() != 0) {
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            getSupportLoaderManager().restartLoader(0, queryBundle, this);
+
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            // Otherwise update the TextView to tell the user there is no connection,
+            // or no search term.
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
+    }
+
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        String queryString = "";
+
+        if (args != null) {
+            queryString = args.getString("queryString");
+        }
+
+        return new BookLoader(this, queryString);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        try {
+            // Convert the response into a JSON object.
+            JSONObject jsonObject = new JSONObject(data);
+            // Get the JSONArray of book items.
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            // Initialize iterator and results fields.
+            int i = 0;
+            String title = null;
+            String authors = null;
+            // Look for results in the items array, exiting when both the title and
+            // are found or when all items have been checked.
+            while (i < itemsArray.length() && (authors == null && title == null)) {
+                // Get the current item information.
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                // Move to the next item.
+                i++;
+            }
+
+            // If both are found, display the result.
+            if (title != null && authors != null) {
+                mTitleText.setText(title);
+                mAuthorText.setText(authors);
+            } else {
+                // If none are found, update the UI to show failed results.
+                mTitleText.setText(R.string.no_results);
+                mAuthorText.setText("");
+            }
+
+        } catch (JSONException e) {
+            // If onPostExecute does not receive a proper JSON string,
+            // update the UI to show failed results.
+            mTitleText.setText(R.string.no_results);
+            mAuthorText.setText("");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
+    }
+}
