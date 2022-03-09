f33b3aa6356f552b24d8bd9572aeb2d2b0479c99
pratch <82988679+pichayakorn@users.noreply.github.com>
2022-03-09 10:57:06 +0700

    final part 2 init

--- Final_Exam/HowFastAreYou/app/src/main/java/com/tni/pratch/howfastareyou/MainActivity.java
+++ Final_Exam/HowFastAreYou/app/src/main/java/com/tni/pratch/howfastareyou/MainActivity.java
@@ -1,19 +1,23 @@
 package com.tni.pratch.howfastareyou;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CUR_SCORE = "current_score";
     // View members
     private TextView score_tv;
-    private Button start_btn, click_me_btn;
+    private Button start_btn;
     private ProgressBar pb;
+    private ImageView im1, im2, im3, im4, im5;
 
     // game members
     int cur_score = 0;
@@ -26,30 +30,65 @@ public class MainActivity extends AppCompatActivity {
         // Initialize view
         score_tv = findViewById(R.id.score_textview);
         start_btn = findViewById(R.id.start_btn);
-        click_me_btn = findViewById(R.id.click_me_btn);
         pb = findViewById(R.id.progressBar);
+        im1 = findViewById(R.id.imgV_1);
+        im2 = findViewById(R.id.imgV_2);
+        im3 = findViewById(R.id.imgV_3);
+        im4 = findViewById(R.id.imgV_4);
+        im5 = findViewById(R.id.imgV_5);
 
-        // init reset score
-        score_tv.setText(R.string.zero);
-        // hide click mew button
-        click_me_btn.setVisibility(View.GONE);
+        // reset game initial value
+        resetGame();
+
+        // Restore current score
+        if (savedInstanceState != null) {
+            String cur_score_str = savedInstanceState.getString(CUR_SCORE);
+            cur_score = Integer.parseInt(cur_score_str);
+            score_tv.setText(cur_score_str);
+        }
 
         start_btn.setOnClickListener(view -> {
             // toggle btn
-            start_btn.setVisibility(View.GONE);
-            click_me_btn.setVisibility(View.VISIBLE);
+            start_btn.setVisibility(View.INVISIBLE);
 
             // reset score
             cur_score = 0;
             score_tv.setText(R.string.zero);
-            new startCountingTask(pb, start_btn, click_me_btn).execute();
+            new startCountingTask(pb, start_btn, im1, im2, im3, im4, im5).execute();
         });
 
-        click_me_btn.setOnClickListener(view -> {
-            // increment score count by 1
-            cur_score++;
-            String cur_score_str = Integer.toString(cur_score);
-            score_tv.setText(cur_score_str);
-        });
+        // Set onclick to imgView
+        im1.setOnClickListener(this::doWhatImgDo);
+        im2.setOnClickListener(this::doWhatImgDo);
+        im3.setOnClickListener(this::doWhatImgDo);
+        im4.setOnClickListener(this::doWhatImgDo);
+        im5.setOnClickListener(this::doWhatImgDo);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String cur_score_str = Integer.toString(cur_score);
+        outState.putString(CUR_SCORE, cur_score_str);
+    }
+
+    private void resetGame() {
+        // init reset score
+        score_tv.setText(R.string.zero);
+
+        // init set visible on image
+        im1.setVisibility(View.INVISIBLE);
+        im2.setVisibility(View.INVISIBLE);
+        im3.setVisibility(View.INVISIBLE);
+        im4.setVisibility(View.INVISIBLE);
+        im5.setVisibility(View.INVISIBLE);
+    }
+
+    private void doWhatImgDo(View v) {
+        v.setVisibility(View.INVISIBLE);
+        // increment score count by 1
+        cur_score++;
+        String cur_score_str = Integer.toString(cur_score);
+        score_tv.setText(cur_score_str);
     }
 }
--- Final_Exam/HowFastAreYou/app/src/main/java/com/tni/pratch/howfastareyou/startCountingTask.java
+++ Final_Exam/HowFastAreYou/app/src/main/java/com/tni/pratch/howfastareyou/startCountingTask.java
@@ -3,30 +3,42 @@ package com.tni.pratch.howfastareyou;
 import android.os.AsyncTask;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import java.lang.ref.WeakReference;
+import java.util.Random;
 
 public class startCountingTask extends AsyncTask<Void, Integer, Void> {
 
+    private static final int NOT_GENERATE = 99;
     // Weak ref
     private WeakReference<ProgressBar> pb;
     private WeakReference<Button> start_btn;
-    private WeakReference<Button> click_me_btn;
+    private WeakReference<ImageView> im1, im2, im3, im4, im5;
 
     // Update every 100 ms
     private static final int CHUNK_SIZE = 100;
+    // random generate img for 300 ms
+    private static final int generateTime = 300;
 
-    public startCountingTask(ProgressBar pb, Button start_btn, Button click_me_btn) {
+    public startCountingTask(ProgressBar pb, Button start_btn,
+                             ImageView im1, ImageView im2, ImageView im3, ImageView im4, ImageView im5) {
         this.pb = new WeakReference<>(pb);
         this.start_btn = new WeakReference<>(start_btn);
-        this.click_me_btn = new WeakReference<>(click_me_btn);
+        this.im1 = new WeakReference<>(im1);
+        this.im2 = new WeakReference<>(im2);
+        this.im3 = new WeakReference<>(im3);
+        this.im4 = new WeakReference<>(im4);
+        this.im5 = new WeakReference<>(im5);
     }
 
     @Override
     protected Void doInBackground(Void... voids) {
         // count value
-        int countDown = 1000;
+        int countDown = 5000;
+
+        Random r = new Random();
 
         int chunkSize = countDown / CHUNK_SIZE;
         for (int i = 0; i < chunkSize; ++i){
@@ -36,7 +48,10 @@ public class startCountingTask extends AsyncTask<Void, Integer, Void> {
             catch (InterruptedException e){
                 e.printStackTrace();
             }
-            publishProgress(((i + 1) * 100) / chunkSize);
+            if (i % (generateTime / CHUNK_SIZE) == 0)
+                publishProgress(((i + 1) * 100) / chunkSize, r.nextInt(5) + 1);
+            else
+                publishProgress(((i + 1) * 100) / chunkSize, NOT_GENERATE);
         }
         return null;
     }
@@ -45,12 +60,35 @@ public class startCountingTask extends AsyncTask<Void, Integer, Void> {
     protected void onProgressUpdate(Integer... values) {
         super.onProgressUpdate(values);
         pb.get().setProgress(values[0]);
+        switch (values[1]) {
+            case 1:
+                im1.get().setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                im2.get().setVisibility(View.VISIBLE);
+                break;
+            case 3:
+                im3.get().setVisibility(View.VISIBLE);
+                break;
+            case 4:
+                im4.get().setVisibility(View.VISIBLE);
+                break;
+            case 5:
+                im5.get().setVisibility(View.VISIBLE);
+                break;
+            case NOT_GENERATE:
+                break;
+        }
     }
 
     @Override
     protected void onPostExecute(Void unused) {
         super.onPostExecute(unused);
         start_btn.get().setVisibility(View.VISIBLE);
-        click_me_btn.get().setVisibility(View.GONE);
+        im1.get().setVisibility(View.INVISIBLE);
+        im2.get().setVisibility(View.INVISIBLE);
+        im3.get().setVisibility(View.INVISIBLE);
+        im4.get().setVisibility(View.INVISIBLE);
+        im5.get().setVisibility(View.INVISIBLE);
     }
 }
